d70810a2091b0ddff5f6af6526dd76ba3156cc98
victor-kiptoo-001 <veelangat680@gmail.com>
2025-03-17 13:28:04 +0300

    Added contact

---
+++ frontend/src/Pages/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import "./Contact.css";
+import { Button } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <div className="contactContainer">
+      <a className="mailBtn" href="mailto:viahalyadav0987@gmail.com">
+        <Button>Contact: viahalyadav0987@gmail.com</Button>
+      </a>
+    </div>
+  );
+};
+
+export default Contact;
